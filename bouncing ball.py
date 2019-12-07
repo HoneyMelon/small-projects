408f080f4b2b8710a32c410203bc62752b828153
Valentina Vef <lauvef@students.uni-mainz.de>
2019-12-07 15:14:06 +0100

    bounce bounce bounce

--- bouncing ball.py
+++ bouncing ball.py
@@ -3,6 +3,7 @@ import stddraw
 import color
 import keyboard
 import math
+import time
 
 maxRadius = 0.3
 minRadius = 0.1
@@ -20,23 +21,34 @@ def init_stddraw():
 def init_values():
     randx = random.randint(2, 9)
     randy = random.randint(2, 9)
-    speedx = random.randint(5, 15) / 300
-    speedy = random.randint(5, 15) / 300
+    speedx = 0.05
+    speedy = 0.05
     return randx, randy, speedx, speedy
 
 
-def bouncing(x, y, speedx, speedy):
+def bouncing_game(x, y, speedx, speedy):
+    starttime = time.time()
+    timediff = 0
     points = []
     position = [x, y]
+    foodPosition = [random.randint(2, 9), random.randint(2, 9)]
+    foodCounter = 0
     speed = [speedx, speedy]
-    print(position, speed)
     slowmo = False
-    while True:
+    while timediff < 60:
         stddraw.clear(color.DARK_GRAY)
         position[0] += speed[0]
         position[1] += speed[1]
         points.append([position[0], position[1]])
-        if len(points) > 50:
+        stddraw.setPenColor(color.CYAN)
+
+        timediff = time.time() - starttime
+        timediffSeconds = 60 - math.floor(timediff)
+        stddraw.text(9.5, 9.5, f'{foodCounter}')
+        stddraw.text(0.5, 9.5, f'{timediffSeconds}')
+
+        stddraw.filledCircle(foodPosition[0], foodPosition[1], 0.1)
+        if len(points) > foodCounter * 5 + 10:
             points.pop(0)
         for i in range(len(points)):
             rainbowrray = [color.RED, color.ORANGE, color.YELLOW, color.GREEN, color.BLUE, color.PURPLE]
@@ -63,6 +75,13 @@ def bouncing(x, y, speedx, speedy):
             position[1] = 0 + maxRadius
             speed[1] = abs(speed[1] * 0.9)
 
+        squaredDistanceToFood = (position[0] - foodPosition[0]) ** 2 + (position[1] - foodPosition[1]) ** 2
+        if squaredDistanceToFood < 0.1:
+            foodPosition = [random.randint(2, 9), random.randint(2, 9)]
+            foodCounter += 1
+
+
+
         if keyboard.is_pressed('q'):  # if key 'q' is pressed
             slowmo = True
         else:
@@ -73,13 +92,13 @@ def bouncing(x, y, speedx, speedy):
             speed[1] += 0.001
         else:
             speed[1] -= 0.001
-
+    print("Game Over! Dein Highscore:", foodCounter)
 
 balls = []
 
 init_stddraw()
 x, y, speedx, speedy = init_values()
-bouncing(x, y, speedx, speedy)
+bouncing_game(x, y, speedx, speedy)
 
 
 
