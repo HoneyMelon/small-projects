bc88ad368db567b81c79004984b4cc6ff6a6258a
Valentina Vef <lauvef@students.uni-mainz.de>
2020-03-10 20:26:11 +0100

    evening changes

--- billard/ball.py
+++ billard/ball.py
@@ -1,9 +1,8 @@
-import tisch
 import pygame
-
+import math
 
 class Ball:
-    def __init__(self, radius, direction, position, speed, number, colour=(255, 255, 255), full=True, on_field=True):
+    def __init__(self, radius, direction, position, speed, number=None, colour=(255, 255, 255), full=True, on_field=True):
         """
         @param direction: list
         @param position: list
@@ -22,32 +21,34 @@ class Ball:
         self.full = full
         self.on_field = on_field
 
-    def add_number(self, screen, font):
-        screen.blit(font.render(str(self.number), True, (0, 0, 0)), (self.position[0] - self.radius,
-                                                                     (self.position[1] - self.radius) - 5))
+    def draw_number(self, screen, font):
+        if self.number:
+            text = font.render(str(self.number), True, (0, 0, 0))
+            text_rect = text.get_rect(center=(self.position[0], self.position[1] - 1))
+            screen.blit(text, text_rect)
 
-    def draw(self, screen, radius):
+    def draw(self, tisch, font):
         """
         @param radius: float
         @param color: tuple of ints
         """
-        screen = tisch.draw_field()
-        if self.on_field is True:
-            pygame.draw.ellipse(screen, self.colour, pygame.Rect(self.position[0] - self.radius,
-                                                                 self.position[1] - self.radius, radius * 2,
-                                                                 radius * 2))
-        else:
-            self.position[0] = 20
-            self.position[1] = 110
+        screen = tisch.get_screen()
+        ball_rectangle = pygame.Rect(self.position[0] - self.radius, self.position[1] - self.radius,
+                                     self.radius * 2, self.radius * 2)
+        pygame.draw.ellipse(screen, self.colour, ball_rectangle)
+        if not self.full:
+            self.draw_white_border(screen)
+        self.draw_number(screen, font)
 
     def apply_friction(self):
-        friction = 0.0001
+        friction = 0.003
         if self.speed < 0:
             self.speed = 0
         if self.speed > 0:
             self.speed -= friction
 
-    def next_position(self):
+    def next_position(self, tisch):
+        self.normalize_direction()
         x = self.position[0]
         y = self.position[1]
         # print(self.direction)
@@ -55,6 +56,39 @@ class Ball:
         y += self.direction[1] * self.speed
         tisch.check_rebound(self, x, y)
         self.apply_friction()
+
+    def normalize_direction(self):
+        direction_length = math.sqrt(self.direction[0] ** 2 + self.direction[1] ** 2)
+        self.direction[0] = self.direction[0] / direction_length
+        self.direction[1] = self.direction[1] / direction_length
+
+    def move_to_cursor(self, x, y):
+        dx = x - self.position[0]
+        dy = y - self.position[1]
+
+        self.direction[0] = dx
+        self.direction[1] = dy
+        self.speed = 2
+
+    def draw_white_border(self, screen):
+        borders = self.get_white_border_polygons()
+        pygame.draw.polygon(screen, (255, 255, 255), borders[0])
+        pygame.draw.polygon(screen, (255, 255, 255), borders[1])
+
+    def get_white_border_polygons(self):
+        upper_border = []
+        lower_border = []
+        for i in range(45, 136):
+            upper_border.append(self.get_point_on_border(i))
+        for i in range(225, 316):
+            lower_border.append(self.get_point_on_border(i))
+        return upper_border, lower_border
+
+    def get_point_on_border(self, degree):
+        rad = math.radians(degree)
+        dx = math.cos(rad) * self.radius
+        dy = math.sin(rad) * self.radius
+        return self.position[0] + dx, self.position[1] + dy
 
 
 def main():
--- billard/billard.py
+++ billard/billard.py
@@ -1,52 +1,30 @@
-import tisch
+from tisch import Tisch
 from ball import Ball
 import pygame
-
-
-def move_to_cursor(ball):
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        x, y = pygame.mouse.get_pos()
-        new_x = x - ball.position[0]
-        new_y = y - ball.position[1]
-        ball.direction = [new_x, new_y]
-        if new_x > 0:
-            ball.direction[0] = 1
-        elif new_x < 0:
-            ball.direction[0] = -1
-        elif new_y > 0:
-            ball.direction[1] = 1
-        elif new_y < 0:
-            ball.direction = -1
-
 
 pygame.font.init()
 pygame.init()
 done = False
 pygame.event.pump()
-font = pygame.font.SysFont('arialblack', 20)
-screen = tisch.draw_field()
-test_ball = Ball(10, [5, 1], [120, 360], 0.5, 1)
-test_ball.draw(screen, 10)
+font = pygame.font.SysFont('arialblack', 11)
+
+tisch = Tisch()
+white_ball = Ball(10, [5, 1], [120, 360], 0)
+balll = Ball(10, [5, 1], [120, 360], 2, 1, (200, 200, 0))
+ball2 = Ball(10, [5, 1], [50, 360], 2, 2, (100, 100, 255))
+ball3 = Ball(10, [2, 1], [50, 360], 2, 14, (255, 100, 100), False)
+balls = [white_ball, balll, ball2, ball3]
+
 while not done:
-    test_ball.next_position()
-    test_ball.draw(screen, 10)
-    test_ball.add_number(screen, font)
-    tisch.is_in_hole(test_ball)
-    #print(test_ball.on_field)
-    #move_to_cursor(test_ball)
+    tisch.draw_field()
+    for ball in balls:
+        ball.next_position(tisch)
+        ball.draw(tisch, font)
+        tisch.is_in_hole(ball)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            new_x = x - test_ball.position[0]
-            new_y = y - test_ball.position[1]
-            if new_x > 0:
-                test_ball.direction[0] = 1
-            elif new_x < 0:
-                test_ball.direction[0] = -1
-            elif new_y > 0:
-                test_ball.direction[1] = 1
-            elif new_y < 0:
-                test_ball.direction[1] = -1
+            white_ball.move_to_cursor(x, y)
